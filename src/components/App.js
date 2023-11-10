996cd1dba4da18dca80ba5386ab666ddd1165a54
reagan <reaganm746@gmail.com>
2023-11-10 20:42:10 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
+import { username, city, image } from "./data/user";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city} />
+      <About image={image} />
     </div>
   );
 }
